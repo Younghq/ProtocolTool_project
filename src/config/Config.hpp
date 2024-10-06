73374571d5d4397a7aaf5948bd34eae34bcddb17
yok1 <yok1@github.com>
2024-10-06 20:48:26 +0800

    重构config模块

---
+++ src/config/Config.hpp
@@ -0,0 +1,6 @@
+#ifndef _Config_hpp_
+#define _Config_hpp_
+#include "ConfigLoader.hpp"
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include "Config.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "hello world" << std::endl;
     return 0;
 }
